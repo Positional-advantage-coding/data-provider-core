d7be435c4e4e18990803c2babfae661e1fc0f99a
aon <aon@celum.com>
2025-11-29 20:15:40 +0100

    Added a posiblility to return undefined from createEntity method, as I want to indicate if operation was sucessfull

--- src/data-provider.ts
+++ src/data-provider.ts
@@ -3,11 +3,11 @@ import { Entity } from './entity';
 import { EntityConverter } from "./converter";
 
 export abstract class DataProvider {
-    public abstract converterMap: Map<string, EntityConverter<any, any>>
+    public abstract converterMap: Map<string, EntityConverter<any, any>>;
 
     public abstract getEntity<T extends Entity<string>>(path: string): Observable<T | undefined>;
 
-    public abstract createEntity<T extends Entity<string>>(path: string, entity: T): Observable<T>;
+    public abstract createEntity<T extends Entity<string>>(path: string, entity: T): Observable<T | undefined>;
 
     public abstract listenToCollectionChanges<T extends Entity<string>>(path: string): Observable<T[]>;
 
