760863278a5f3460080625aafc8616dcd740cc45
aon <aon@celum.com>
2025-11-29 21:12:43 +0100

    connected to idGenerator

--- src/data-provider.ts
+++ src/data-provider.ts
@@ -1,8 +1,11 @@
 import { Observable } from 'rxjs';
 import { Entity } from './entity';
 import { EntityConverter } from "./converter";
+import {IdGenerator} from "@positional_advantage_coder/id-generator";
 
 export abstract class DataProvider {
+    public abstract idGenerator: IdGenerator;
+
     public abstract converterMap: Map<string, EntityConverter<any, any>>;
 
     public abstract getEntity<T extends Entity<string>>(path: string): Observable<T | undefined>;
