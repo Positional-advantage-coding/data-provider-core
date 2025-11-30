26a4e17f61c025430e7766ba43cd0f368029ab56
aon <aon@celum.com>
2025-11-30 17:52:35 +0100

    removed ambiguous validateCreation to the internal parts of fromPlainObject() method and add createDraft() method

--- src/converter.ts
+++ src/converter.ts
@@ -1,9 +1,10 @@
 import { Entity } from './entity';
+import {IdGenerator} from "@positional_advantage_coder/id-generator";
 
 export abstract class EntityConverter<ID = string, T extends Entity<ID> = Entity<ID>> {
     abstract toPlainObject(entity: T): object;
     abstract fromPlainObject(value: any): T | null;
-    abstract validateCreation(value: any): boolean;
+    abstract createDraft(data: Omit<T, 'id' | 'typeKey'>): Omit<T, 'id'>;
 }
 
 export interface EntityConverterConfig {
--- src/data-provider.ts
+++ src/data-provider.ts
@@ -10,7 +10,7 @@ export abstract class DataProvider {
 
     public abstract getEntity<T extends Entity<string>>(path: string): Observable<T | undefined>;
 
-    public abstract createEntity<T extends Entity<string>>(path: string, entity: T): Observable<T | undefined>;
+    public abstract createEntity<T extends Entity<string>>(path: string, draftEntity: Omit<T, 'id'>): Observable<T | undefined>;
 
     public abstract listenToCollectionChanges<T extends Entity<string>>(path: string): Observable<T[]>;
 
