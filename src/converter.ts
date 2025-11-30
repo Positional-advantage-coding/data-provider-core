d6a051320a8cee93624d0862a0d8c1fd3e957ce5
aon <aon@celum.com>
2025-11-30 18:53:24 +0100

    adapted the contract types

--- src/converter.ts
+++ src/converter.ts
@@ -2,8 +2,8 @@ import { Entity } from './entity';
 
 export abstract class EntityConverter<ID = string, T extends Entity<ID> = Entity<ID>> {
     abstract toPlainObject(entity: T): object;
-    abstract fromPlainObject(value: any): T | null;
-    abstract createDraft(data: Omit<T, 'id' | 'typeKey'>): Omit<T, 'id'>;
+    abstract fromPlainObject(value: any): T | undefined;
+    abstract createDraft(data: Omit<T, 'id' | 'typeKey'>): Omit<T, 'id'> | undefined;
 }
 
 export interface EntityConverterConfig {
